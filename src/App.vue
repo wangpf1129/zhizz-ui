8287a903c28da2af2a454a5e4e562fa21367b300
Wangpf <1204981117@qq.com>
2021-02-25 17:25:10 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <wang/>
+</template>
+
+<script lang="ts">
+  import Wang from './components/Wang.vue';
+
+  export default {
+    name: 'App',
+    components: {Wang},
+  };
+</script>
---
+++ src/components/Wang.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>hello,vue3</div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Wang',
+  };
+</script>
+
