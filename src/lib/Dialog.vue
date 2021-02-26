f7ca9d2e3cd242812d263579bd31c7be6606c321
Wangpf <1204981117@qq.com>
2021-02-26 18:36:07 +0800

    Dialog组件样式

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="z-dialog-overlay"></div>
   <div class="z-dialog-wrapper">
-    <header>标题</header>
-    <main>
-      <p>第一行</p>
-      <p>第二行</p>
-    </main>
-    <footer>
-      <Button>取消</Button>
-      <Button theme="primary">确认</Button>
-    </footer>
+    <div class="z-dialog">
+      <header>
+        标题
+        <span class="z-dialog-close"></span>
+      </header>
+      <main>
+        <p>第一行11111111111111111111111111111111111111</p>
+        <p>第二行</p>
+      </main>
+      <footer>
+        <Button>取消</Button>
+        <Button theme="primary">确认</Button>
+      </footer>
+    </div>
   </div>
 </template>
 
@@ -24,5 +29,77 @@
 </script>
 
 <style lang="scss" scoped>
+  $radius: 4px;
+  $border-color: #d9d9d9;
+  .z-dialog {
+    background: white;
+    border-radius: $radius;
+    box-shadow: 0 0 3px fade_out(black, 0.5);
+    min-width: 15em;
+    max-width: 90%;
 
+    &-overlay {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: fade_out(black, 0.5);
+      z-index: 10;
+    }
+
+    &-wrapper {
+      position: fixed;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+      z-index: 11;
+    }
+
+    > header {
+      padding: 12px 16px;
+      border-bottom: 1px solid $border-color;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      font-size: 20px;
+    }
+
+    > main {
+      padding: 12px 16px;
+    }
+
+    > footer {
+      border-top: 1px solid $border-color;
+      padding: 12px 16px;
+      text-align: right;
+    }
+
+    &-close {
+      position: relative;
+      display: inline-block;
+      width: 16px;
+      height: 16px;
+      cursor: pointer;
+
+      &::before,
+      &::after {
+        content: '';
+        position: absolute;
+        height: 1px;
+        background: black;
+        width: 100%;
+        top: 50%;
+        left: 50%;
+      }
+
+      &::before {
+        transform: translate(-50%, -50%) rotate(-45deg);
+      }
+
+      &::after {
+        transform: translate(-50%, -50%) rotate(45deg);
+      }
+    }
+  }
 </style>
