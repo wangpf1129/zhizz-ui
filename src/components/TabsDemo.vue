07df3ba1a6b2f8994b8f1efade0e95fa0f440670
Wangpf <1204981117@qq.com>
2021-02-27 14:04:16 +0800

    创建Tabs 和 TabItem 组件

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,11 +1,18 @@
 <template>
-
-
+  <h2>Tabs示例</h2>
+  <Tabs>
+    <tab-item title="学习">学习</tab-item>
+    <tab-item title="吃饭">吃饭</tab-item>
+    <tab-item title="睡觉">睡觉</tab-item>
+  </Tabs>
 </template>
 
 <script lang="ts">
+  import Tabs from '../lib/Tabs.vue';
+  import TabItem from '../lib/TabItem.vue';
   export default {
     name: 'TabsDemo',
+    components: {TabItem, Tabs},
   };
 </script>
 
---
+++ src/lib/TabItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>tab-item.vue</div>
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'TabItem',
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
