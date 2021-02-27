76a805dd120bbd2f822490dc2edcc97efdd48260
Wangpf <1204981117@qq.com>
2021-02-27 20:48:31 +0800

    首页 样式

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,6 +1,10 @@
 <template>
   <section class="top_nav">
-    <div class="logo">Logo</div>
+    <div class="logo">
+      <svg class="icon">
+        <use xlink:href="#icon-lanmei"></use>
+      </svg>
+    </div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
@@ -10,72 +14,83 @@
 </template>
 
 <script lang="ts">
-  import {inject, Ref} from 'vue';
+import {inject, Ref} from 'vue';
 
-  export default {
-    name: 'TopNav',
-    setup() {
-      const asideVisible = inject<Ref<boolean>>('asideVisible');
-      const toggleAside = () => {
-        asideVisible.value = !asideVisible.value;
-      };
+export default {
+  name: 'TopNav',
+  setup() {
+    const asideVisible = inject<Ref<boolean>>('asideVisible');
+    const toggleAside = () => {
+      asideVisible.value = !asideVisible.value;
+    };
 
-      return {toggleAside};
-    }
-  };
+    return {toggleAside};
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-  .top_nav {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    background-color: #e9dcfb;
-    display: flex;
-    padding: 16px;
-    justify-content: center;
-    align-items: center;
-    z-index: 10;
+$buttonColor: #0864a9;
+$textColor: #05538c;
+.top_nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  color: $textColor;
+  display: flex;
+  padding: 16px;
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
+  box-shadow: 0 5px 5px rgb(51 51 51 / 10%);
 
-    > .logo {
-      max-width: 6em;
-      margin-right: auto;
-    }
+  > .logo {
+    margin-left: 50px;
+    cursor: pointer;
+    max-width: 6em;
+    margin-right: auto;
 
-    .menu {
-      display: flex;
-      white-space: nowrap;
-      flex-wrap: nowrap;
-
-      > li {
-        margin: 0 1em;
-      }
-    }
-
-    > .toggleAside {
-      display: none;
-      width: 24px;
-      height: 24px;
-      background-color: red;
-      position: absolute;
-      top: 50%;
-      left: 16px;
-      transform: translateY(-50%);
-
-    }
-
-    @media (max-width: 500px) {
-      > .menu {
-        display: none;
-      }
-      > .logo {
-        margin-left: auto;
-        margin-right: auto;
-      }
-      > .toggleAside {
-        display: inline-block;
-      }
+    svg {
+      width: 38px;
+      height: 38px;
     }
   }
+
+  .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+
+    > li {
+      margin: 0 1em;
+    }
+  }
+
+  > .toggleAside {
+    display: none;
+    width: 24px;
+    height: 24px;
+    background-color: red;
+    position: absolute;
+    top: 50%;
+    left: 16px;
+    transform: translateY(-50%);
+
+  }
+
+  @media (max-width: 500px) {
+    > .menu {
+      display: none;
+    }
+    > .logo {
+      margin-left: auto;
+      margin-right: auto;
+    }
+    > .toggleAside {
+      display: inline-block;
+    }
+  }
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,158 @@
 <template>
-  <TopNav/>
-  <section class="banner">
-    <h1>Zhizz UI</h1>
-    <h2>一个好看的 UI 框架</h2>
-    <p class="actions">
-      <a target="_blank" href="https://github.com/wwwpppfffzzz">GitHub</a>
-      <router-link to="/doc">Start</router-link>
-    </p>
-  </section>
+  <div class="home">
+    <TopNav/>
+    <div>
+      <section class="banner">
+        <h1>Zhizz UI框架</h1>
+        <h2>简约而又不失创造力</h2>
+        <div class="actions">
+          <a target="_blank" href="https://github.com/wwwpppfffzzz">GitHub</a>
+          <router-link to="/doc">开始</router-link>
+        </div>
+      </section>
+      <div class="features">
+        <ul>
+          <li>
+            <svg class="icon">
+              <use xlink:href="#icon-vue"></use>
+            </svg>
+            <h3>基于 Vue 3</h3>
+            <p>使用了 Vue 3 Composition API</p>
+          </li>
+          <li>
+            <svg class="icon">
+              <use xlink:href="#icon-ts"></use>
+            </svg>
+            <h3>基于 TypeScript </h3>
+            <p>源代码采用 TypeScript 书写（非严格检查）</p>
+          </li>
+          <li>
+            <svg class="icon">
+              <use xlink:href="#icon-led"></use>
+            </svg>
+            <h3>代码易读</h3>
+            <p>每个组件的源代码都极其简洁</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script lang="ts">
-  import TopNav from '../components/TopNav.vue';
+import TopNav from '../components/TopNav.vue';
 
-  export default {
-    name: 'Home',
-    components: {TopNav},
-  };
+export default {
+  name: 'Home',
+  components: {TopNav},
+};
 </script>
 
 <style lang="scss" scoped>
+$buttonColor: #0864a9;
+$textColor: #05538c;
+
+.home {
+  ::v-deep(.top_nav) {
+    background: transparent;
+  }
+
   .banner {
-    padding: 100px 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    background-color: #dccdf2;
+    padding: 180px 0 150px;
+    color: $textColor;
+    background: rgb(174, 209, 228);
+    background: linear-gradient(145deg, rgba(174, 209, 228, 1) 0%, rgba(66, 125, 169, 1) 100%);
+    clip-path: ellipse(80% 60% at 50% 40%);
+    margin: 0 auto;
+
+    h1 {
+      text-align: center;
+      font-size: 34px;
+      line-height: 52px;
+    }
+
+    h2 {
+      text-align: right;
+      padding-right: 100px;
+      margin-top: 12px;
+      font-size: 20px;
+      line-height: 26px;
+    }
 
     > .actions {
       padding: 8px 0;
+      font-size: 18px;
+      margin-top: 16px;
+      text-align: center;
 
       a {
         margin: 0 8px;
-        background: #6e02c3;
-        padding: 8px 20px;
+        background: $buttonColor;
+        padding: 8px 26px;
         color: #fff;
-        border-radius: 8px;
-        $h: 28px;
-        height: $h;
-        line-height: $h;
+        border-radius: 6px;
+        border: none;
+      }
+
+    }
+  }
+
+  .features {
+
+    margin: 64px auto;
+    width: 300px;
+    @media (min-width: 800px) {
+      width: 800px;
+      h3 {
+        font-size: 20px;
+      }
+
+      p {
+        font-size: 14px;
+      }
+    }
+    @media (min-width: 1200px) {
+      width: 1200px;
+    }
+
+    > ul {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+
+      > li {
+
+        width: 300px;
+        margin: 16px 0px;
+        display: grid;
+        justify-content: start;
+        align-content: space-between;
+        grid-template-areas:
+        "icon title"
+        "icon text";
+        grid-template-columns: 80px auto;
+        grid-template-rows: 1fr auto;
+
+        > svg {
+          grid-area: icon;
+          width: 64px;
+          height: 64px;
+        }
+
+        > h3 {
+          grid-area: title;
+          font-size: 28px;
+        }
+
+        > p {
+          grid-area: text;
+          font-size: 18px;
+        }
       }
     }
   }
+}
+
+
 </style>
