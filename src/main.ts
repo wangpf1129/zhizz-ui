131cb22731c26d01d16bfa2ca04a6c758d153c99
Wangpf <1204981117@qq.com>
2021-02-27 22:37:39 +0800

    markdown css

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import {createApp} from 'vue';
 import './_z.scss';
 import './index.scss';
-
+import 'github-markdown-css';
 import App from './App.vue';
 import {router} from './router';
 
--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,5 +1,15 @@
 <template>
-<div>Intro.vue</div>
+<article class="markdown-body">
+  <h1>安装</h1>
+  <p>打开终端</p>
+  <pre><code>npm install Zhizz-ui</code></pre>
+  <p>或</p>
+  <pre><code>yarn add install Zhizz-ui</code></pre>
+
+  <p>
+    下一节： <a href="#/doc/start">开始使用</a>
+  </p>
+</article>
 </template>
 
 <script lang="ts">
