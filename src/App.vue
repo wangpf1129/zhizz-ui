62aa5e5807db7c7674fe18e1ec46539f546be992
Wangpf <1204981117@qq.com>
2021-03-01 17:16:36 +0800

    加个进度条样式， 完善文档内容

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,42 @@
 <script lang="ts">
   import {ref, provide} from 'vue';
   import {router} from './router';
+  import progress from 'nprogress';
+  import 'nprogress/nprogress.css'
 
+  console.log(progress);
   export default {
     name: 'App',
     setup() {
       const width = document.documentElement.clientWidth;
       const asideVisible = ref(width > 500);
+
+      router.beforeEach((to,from,next)=>{
+        progress.start()
+        next()
+      })
       router.afterEach(() => {
         if (width <= 500) {
           asideVisible.value = false;
         }
+        progress.done()
+        window.scrollTo(0,0)
       });
 
       provide('asideVisible', asideVisible);
     }
   };
 </script>
+
+
+<style lang="scss">
+#nprogress {
+  .bar {
+    background: #6b9ab8 !important; //自定义颜色
+  }
+
+  .spinner-icon {
+    border-color: #e5f2fa transparent transparent #e5f2fa;
+  }
+}
+</style>
--- src/markdown/start.md
+++ src/markdown/start.md
@@ -4,7 +4,7 @@
 然后在你的代码中写入下面的代码
 
 ```
-import {Button, Tabs, Switch, Dialog} from "king-ui"
+import {Button, Tabs, Switch, Dialog} from "zhizz-ui"
 ```
 
 就可以使用我提供的组件了。
@@ -20,7 +20,7 @@ import {Button, Tabs, Switch, Dialog} from "king-ui"
   </div>
 </template>
 <script>
-import {Button, Tabs, Switch, Dialog} from "king-ui"
+import {Button, Tabs, Switch, Dialog} from "zhizz-ui"
 export default {
   components: {Button}
 }
